bd0db4e6f95ac9542fb11b9200a5006b13259621
roman <roman.robel@exitb.de>
2018-08-28 14:32:22 +0200

    * config

--- app.js
+++ app.js
@@ -120,6 +120,30 @@
     }
 
 /*******************************
+ * shutdown
+ *******************************/
+function shutDown() {
+    console.log('Received kill signal, shutting down gracefully');
+    strip.Clear();
+
+    server.close(() => {
+        console.log('Closed out remaining connections');
+        process.exit(0);
+    });
+
+    setTimeout(() => {
+        console.error('Could not close connections in time, forcefully shutting down');
+        process.exit(1);
+    }, 10000);
+
+    connections.forEach(curr => curr.end());
+    setTimeout(() => connections.forEach(curr => curr.destroy()), 5000);
+}
+
+process.on('SIGTERM', shutDown);
+process.on('SIGINT', shutDown);
+
+/*******************************
     startup
 *******************************/
 const server = app.listen(HTTP_PORT, function() {
@@ -128,6 +152,8 @@ const server = app.listen(HTTP_PORT, function() {
     console.log('***************************');
 });
 
-setInterval(() => server.getConnections(
-    (err, connections) => console.log(`${connections} connections currently open`)
-), 1000);
+let connections = [];
+server.on('connection', connection => {
+    connections.push(connection);
+    connection.on('close', () => connections = connections.filter(curr => curr !== connection));
+});
