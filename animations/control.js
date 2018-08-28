f50c9a1e9b3cd062f6522d4d843b9caa84c921cc
roman <roman.robel@exitb.de>
2018-08-28 15:04:38 +0200

    * config

--- animations/control.js
+++ animations/control.js
@@ -14,7 +14,7 @@
 
         this.Single = function(args, strip) {
             strip.SetLightColor(args.lightIndex, args.colorRed, args.colorGreen, args.colorBlue);
-            strip.SetBrightness(args.brightness);
+
             strip.Mode = "FOO";
             console.log("set single color");
         };
