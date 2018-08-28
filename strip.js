3fbcfc0fde4e99d537e9058f0e720b176ad73c63
roman <roman.robel@exitb.de>
2018-08-28 13:53:37 +0200

    * config

--- strip.js
+++ strip.js
@@ -3,15 +3,24 @@ var ws281x = require('rpi-ws281x-native');
 
 ws281x.init(config.NUM_LED);
 
+var Lights = [];
+
 function strip() {
+
+    this.NUM_LEDS = config.NUM_LED;
+    this.Mode = "";
     this.Lights = [];
 
     this.Clear = function () {
         ws281x.reset();
     };
 
+    /*
+    *   Clear all LEDs back to 0x00000 and render
+    */
     this.Stop = function () {
         this.Clear();
+        CurrentMode = MODES.CLEAR;
     };
 
     /*
@@ -25,19 +34,18 @@ function strip() {
     *   Set a single color for all LEDs
     */
     this.SetStripColor = function (color) {
-        for (var i = 0; i < config.NUM_LED; i++) {
+        for (var i = 0; i < this.NUM_LEDS; i++) {
             this.Lights[i] = color;
         }
-
         this.Render();
     };
 
     this.Render = function () {
-        var tmp = [];
-        for (var i = 0; i < config.NUM_LED; i++) {
-            if (i > config.NUM_LED) break;
+        let tmp = [];
+        for (let i = 0; i < this.NUM_LEDS; i++) {
             tmp[i] = this.Lights[i];
         }
+
         ws281x.render(tmp);
     };
 }
